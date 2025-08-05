6b1f7fcb78ce19b7fd474743356c9796a7fd61a0
cordovabarbara <aliendocordova@gmail.com>
2025-08-05 12:16:54 -0500

    Update task_list and create view to filter completed and pending tasks

--- crudproject/crud/views.py
+++ crudproject/crud/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
 def task_list_and_create(request):
-    form = TaskForm()
-    tasks = Task.objects.all()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crud:crud_list')
+    else:
+        form = TaskForm()
+    #tasks = Task.objects.all()
+    completed_tasks = Task.objects.filter(completed=True)
+    incompleted_tasks = Task.objects.filter(completed=False)
 
     return render(request, 'task_list.html',{
         'form':form,
-        'tasks':tasks
+        'completed_tasks':completed_tasks,
+        'incompleted_tasks': incompleted_tasks
+        
+        
+        #'tasks':tasks
     })
