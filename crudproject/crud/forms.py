4faff348a923e06f56947b790b4822f4a399831a
cordovabarbara <aliendocordova@gmail.com>
2025-08-06 12:46:45 -0500

    Apply Tailwind styles to task list layout

--- crudproject/crud/forms.py
+++ crudproject/crud/forms.py
@@ -5,3 +5,12 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title','description']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'w-full 768px; border border-black rounded px-3 py-2'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'w-full border border-black rounded px-3 py-2',
+                'rows': 4
+            })
+        }
