c4f0a97914f6b48e7b131e0ff61800fea797507c
cordovabarbara <aliendocordova@gmail.com>
2025-08-07 12:14:55 -0500

    Add task editing functionality and update edit button style

--- crudproject/crud/views.py
+++ crudproject/crud/views.py
@@ -29,3 +29,16 @@ def update_task(request, task_id):
         task.completed = not task.completed
         task.save()
         return redirect('crud:crud_list')
+    
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('crud:crud_list')
+    else:
+        form = TaskForm(instance=task)
+        
+    return render(request,'edit_task.html', {'form':form})
