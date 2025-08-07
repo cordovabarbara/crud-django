252ae3c859009c93ee0b35e6be48bfb9a0704d4f
cordovabarbara <aliendocordova@gmail.com>
2025-08-07 11:06:31 -0500

    Improve UI layout: added spacing between columns and applied gradient background

--- crudproject/crud/views.py
+++ crudproject/crud/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
@@ -16,9 +17,15 @@ def task_list_and_create(request):
 
     return render(request, 'task_list.html',{
         'form':form,
+        #'tasks':tasks
         'completed_tasks':completed_tasks,
         'incompleted_tasks': incompleted_tasks
-        
-        
-        #'tasks':tasks
     })
+    
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    
+    if request.method == 'POST':
+        task.completed = not task.completed
+        task.save()
+        return redirect('crud:crud_list')
